b6ae80be7785fe4f21cf5f92c5332bdd9f3e64b7
fuhao <3144941746@qq.com>
2025-10-16 12:29:58 +0800

    2090

---
+++ src/No2090.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class No2090 {
+    public int[] getAverages(int[] nums, int k) {
+        int[] avgs = new int[nums.length];
+        long sum = 0;
+
+        Arrays.fill(avgs,-1);
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+            if(i<2*k) continue;
+            avgs[i-k] = (int) (sum / (2*k+1));
+            sum -= nums[i-2*k];
+        }
+
+        return avgs;
+    }
+}
