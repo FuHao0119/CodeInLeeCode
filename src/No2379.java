19093f80765a92a3271e5ce6ff92610d72dd9e92
Fuhao <3144941746@qq.com>
2025-10-20 12:28:04 +0800

    update

---
+++ src/No2379.java
@@ -0,0 +1,32 @@
+public class No2379 {
+    public int minimumRecolors(String blocks, int k) {
+        char[] blocksArr = blocks.toCharArray();
+        int l = 0; int r = 0;
+
+        int maxCount = 0; int count = 0;
+
+        while(r<k-1){
+            if(blocksArr[r] == 'B') {
+                count++;
+            }
+            r++;
+            maxCount = Math.max(maxCount,count);
+        }
+
+        while(r<blocksArr.length)
+        {
+
+            if(blocksArr[r] == 'B') {
+                count++;
+            }
+            r++;
+            if(blocksArr[l] == 'B'){
+                count--;
+            }
+            l++;
+            maxCount = Math.max(maxCount,count);
+        }
+
+        return k - maxCount;
+    }
+}
