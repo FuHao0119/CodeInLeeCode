44fd0f23be0ae72e4007195084db6db330857b9e
Fuhao <3144941746@qq.com>
2025-11-05 12:44:47 +0800

    update

---
+++ src/No49.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class No49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str: strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if(map.containsKey(sortedStr)){
+                map.get(sortedStr).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sortedStr,list);
+            }
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry: map.entrySet()){
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = new No49().groupAnagrams(strs);
+        System.out.println(lists);
+    }
+}
