df4d1cf1ba1afaf0f31b9287967463fbf2ab86b0
Fuhao <3144941746@qq.com>
2025-11-14 12:03:21 +0800

    update

---
+++ src/No303.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class No303 {
+        int[] arr = null;
+        int[] sum_arr = null;
+
+        public void NumArray(int[] nums) {
+            arr = nums;
+            sum_arr = nums;
+            int sum = 0;
+            for(int i = 0; i < nums.length; i++){
+                sum += arr[i];
+                sum_arr[i] = sum;
+            }
+            System.out.println(Arrays.toString(sum_arr));
+        }
+
+        public int sumRange(int left, int right) {
+            return sum_arr[right] - (
+                    left-1<0 ? 0 : sum_arr[left-1]
+            );
+        }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+}
