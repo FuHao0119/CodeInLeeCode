c031e0ce1f069cc793f635222a6d4485129a36e8
Fuhao <3144941746@qq.com>
2025-11-19 14:39:14 +0800

    update

---
+++ src/No2154.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class No2154 {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num: nums){
+            set.add(num);
+        }
+        while(set.contains(original)){
+            original *= 2;
+        }
+        return original;
+    }
+}
